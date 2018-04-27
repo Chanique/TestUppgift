777457ae07c54e97735984fd080ec47e92543b84
Unknown <chanique.borjesson.back@hotmail.com>
2018-04-27 14:57:47 +0200

    Skeleton for the webpage

---
+++ kontakt.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="sv">
+  <head>
+    <title> Jannes Inredning AB - Kontakt </title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+  <body>
+    <div class="header" align="center">
+      <img src="pics/logo.png">
+    </div>
+    <div class="navbar">
+      <ul>
+        <li><a href="start.php"> Start </a></li>
+        <li><a href="tjanster.php"> Tjänster </a></li>
+        <li><a href="portfolio.php"> Portfolio </a></li>
+        <li><a href="kontakt.php"> Kontakt </a></li>
+      </ul>
+    </div>
+    <div class="content">
+      <h2> Kontakt </h2>
+    </div>
+  </body>
+</html>
---
+++ tjanster.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="sv">
+  <head>
+    <title> Jannes Inredning AB - Tjänster </title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+  <body>
+    <div class="header" align="center">
+      <img src="pics/logo.png">
+    </div>
+    <div class="navbar">
+      <ul>
+        <li><a href="start.php"> Start </a></li>
+        <li><a href="tjanster.php"> Tjänster </a></li>
+        <li><a href="portfolio.php"> Portfolio </a></li>
+        <li><a href="kontakt.php"> Kontakt </a></li>
+      </ul>
+    </div>
+    <div class="content">
+      <div class="side">
+        <p> Lista </p>
+      </div>
+      <div class="main">
+        <h4> Tjänster </h4>
+      </div>
+    </div>
+  </body>
+</html>
